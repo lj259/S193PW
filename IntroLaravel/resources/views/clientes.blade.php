2ceb6d0f569307a9b8a48e1b0634dfc166fa09ab
lj259 <120034783@upq.edu.mx>
2024-09-26 19:12:20 -0600

    Finalizacion practica 3

---
+++ IntroLaravel/resources/views/clientes.blade.php
@@ -0,0 +1,19 @@
+@extends('layouts.plantilla')
+
+@section('contenido')
+  <div class="container mt-5 col-md-8">
+    <div class="card text-justify font-monospace">
+      <div class="card-header fs-5 text-primary">
+        Ivan Tisoy Guerra <!--Se deja?-->
+      </div>
+      <div class="card-body">
+        <h5 class="fw-bold">ivan.guerrae@hot.com</h5>
+        <p class="card-text fw-lighter">4422830365</p>
+      </div>
+      <button type="submit" class="btn btn-warning btn-sm">Actualizar</button>
+      <button type="submit" class="btn btn-danger btn-sm">Eliminar</button>
+    </div>
+  </div>
+</body>
+</html>
+@endsection
---
+++ IntroLaravel/resources/views/inicio.blade.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Inicio</title>
+    @vite(['resources/js/app.js'])
+    <style>
+        body, html {
+            height: 100%;
+        }
+        .full-height {
+            height: 100vh;
+        }
+    </style>
+</head>
+<body>
+    <div class="d-flex flex-column justify-content-center align-items-center text-center full-height">
+        <h1 class="display-1">Bienvenido Turista!</h1>
+        <p>Presiona el botón para iniciar...</p>
+        <a href="/form"  class="btn btn-primary">Ir al Registro</a>
+        <a href="{{ route('rutaForm') }}" class="btn btn-danger">Ir al Registro</a>
+    </div>
+</body>
+</html>
